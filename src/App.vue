57b790f1bf4dab1f6c775cc07e28e2ea6f0729ed
brayandotor <144272698+brayandotor@users.noreply.github.com>
2023-09-25 18:33:57 +0000

    🌟 Feature: Shopping Cart App Agrega icono de titulo usando gooogle fonts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
+import {ref} from 'vue';
+const header =ref ( 'Mi carrito de compras')
 
 </script>
 
 <template>
- <h1>{Mi carrito de compras}</h1>
+ <h1><i class="material-icons shopping-cart-icon">local_mall</i>{{header}}</h1>
 </template>
 
 <style scoped>
-
+.shopping-cart-icon{
+font-size: 2rem;}
 </style>
