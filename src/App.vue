8e9466dbb48fefd88bdfde2e903326a4616e46e0
brayandotor <144272698+brayandotor@users.noreply.github.com>
2023-10-09 18:47:34 +0000

    🚧 Construction. cancel button

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,28 @@
 import {ref} from 'vue';
 const header =ref ( 'App lista de compras')
 const items = ref([
-{id: 1, label: '10 bolillos'},
-{id: 2, label: '1 lata de frijoles'},
-{id: 3, label: '1 lata de atún'},
+// {id: 1, label: '10 bolillos'},
+//{id: 2, label: '1 lata de frijoles'},
+//{id: 3, label: '1 lata de atún'},
 ]);
+//agregando metodo para guardar nuevo articulo en la lista
+const saveItem = () => {
+items.value.push ({id: items.value.lenght + 1, label: newItem.value}) 
+//limpiando el contenido de newItem
+newItem.value ="";
+};
 const newItem = ref ('');
 const newItemPriority = ref (false);
 </script>
 
 <template>
- <h1><i class="material-icons shopping-cart-icon">local_mall</i>{{header}}</h1>
- <form v-on:submit.prevent="items.push ({id: items.lenght +1, label: newItem}) " class="add-item form">
+   <div class="header">
+
+      <h1><i class="material-icons shopping-cart-icon">local_mall</i>{{header}}</h1>
+      <button class="btn">cancelar</button>
+      <button class="btn btn-primary">Agregar Articulo</button>
+   </div>
+ <form v-on:submit.prevent="saveItem" class="add-item form">
     <!--Input de nuevo articulo-->
     <input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo">
     <!--check Boxes-->
@@ -29,6 +40,8 @@ const newItemPriority = ref (false);
     {{label}}
     </li>
  </ul>
+ <p v-if="items.length === 0">🥀 Lista de compras vacia 🥀</p>
+ <p v-else>🔥 ingrese mas items 🔥</p>
 </template>
 
 <style scoped>
